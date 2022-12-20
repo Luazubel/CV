15199b520eb530096be98f814fb4e49991c0c20f
Lucia Azubel <Lucia_24_azubel@hotmail.com>
2022-12-19 21:17:25 -0300

    agrego las validaciones de todos los campos

--- js/validacion.js
+++ js/validacion.js
@@ -1,13 +1,63 @@
 let enviar = document.getElementById("enviar");
+let nombre = document.getElementById("nombre");
+let empresa = document.getElementById("empresa");
+let email = document.getElementById("email");
+let comentario = document.getElementById("comentario");
 
+//eventos
 enviar.addEventListener("click", enviarInfo);
+nombre.addEventListener("blur", () => {
+  validarNombre(nombre);
+});
+empresa.addEventListener("blur", () => {
+  validarEmpresa(empresa);
+});
+email.addEventListener("blur", () => {
+  validarEmail(email);
+});
+comentario.addEventListener("blur", () => {
+  validarComentario(comentario);
+});
 
-function enviarInfo() {
-  Swal.fire({
-    icon: "success",
-    title: "La informacion de envio",
-    showConfirmButton: false,
-    timer: 1500,
-  });
+//validaciones
+function validarNombre(input) {
+  if (input.value.length >= 3 && input.value.length <= 50) {
+    input.className = "form-control is-valid";
+  } else {
+    input.className = "form-control is-invalid";
+  }
 }
 
+function validarEmpresa(input) {
+  if (input.value.length >= 3 && input.value.length <= 50) {
+    input.className = "form-control is-valid";
+  } else {
+    input.className = "form-control is-invalid";
+  }
+}
+
+function validarEmail(input) {
+    let exp = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if(exp.test (input.value)){
+        input.className = 'form-control is-valid';
+    }else{
+        input.className = 'form-control is-invalid';
+    }
+}
+
+function validarComentario(input) {
+  if (input.value.length >= 30 && input.value.length <= 200) {
+    input.className = "form-control is-valid";
+  } else {
+    input.className = "form-control is-invalid";
+  }
+}
+
+function enviarInfo() {
+        Swal.fire({
+            icon: "success",
+            title: "La informacion de envio",
+            showConfirmButton: false,
+            timer: 3000
+          });   
+}
